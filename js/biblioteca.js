47d887049c24cb63e020c2d50ab5dd3c140571ab
jeremias252 <8f2kfhp522@privaterelay.appleid.com>
2026-07-29 15:35:17 -0300

    teste12

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -2,13 +2,73 @@ function abrirBiblioteca() {
 
     const pagina = document.getElementById("pagina");
 
-    pagina.innerHTML = `
-        <h2>📚 Biblioteca Bíblica</h2>
+    const antigoTestamento = [
+        "Gênesis",
+        "Êxodo",
+        "Levítico",
+        "Números",
+        "Deuteronômio",
+        "Josué",
+        "Juízes",
+        "Rute",
+        "1 Samuel",
+        "2 Samuel"
+    ];
 
-        <p>
-            Aqui ficarão os livros teológicos, comentários,
-            dicionários e materiais de estudo.
-        </p>
+    let html = `
+        <div class="biblioteca">
+
+            <h1>📚 Biblioteca Bíblica</h1>
+
+            <p>Selecione um livro da Bíblia para iniciar o estudo.</p>
+
+            <input
+                type="text"
+                id="pesquisaLivro"
+                placeholder="Pesquisar livro..."
+            >
+
+            <h2>Antigo Testamento</h2>
+
+            <div class="lista-livros">
+    `;
+
+    antigoTestamento.forEach(livro => {
+
+        html += `
+            <div class="livro" onclick="abrirLivro('${livro}')">
+                ${livro}
+            </div>
+        `;
+
+    });
+
+    html += `
+            </div>
+
+        </div>
+    `;
+
+    pagina.innerHTML = html;
+
+}
+function abrirLivro(nomeLivro){
+
+    const pagina = document.getElementById("pagina");
+
+    pagina.innerHTML = `
+        <button onclick="abrirBiblioteca()">⬅ Voltar</button>
+
+        <h1>${nomeLivro}</h1>
+
+        <h3>Autor</h3>
+        <p>Em breve...</p>
+
+        <h3>Data</h3>
+        <p>Em breve...</p>
+
+        <h3>Tema</h3>
+        <p>Em breve...</p>
     `;
 
 }

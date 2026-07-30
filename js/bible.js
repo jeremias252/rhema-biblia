a08c398048716a5f2cc88b1ffc69a8365fb42930
jeremias252 <8f2kfhp522@privaterelay.appleid.com>
2026-07-30 12:53:31 -0300

    d

--- js/bible.js
+++ js/bible.js
@@ -28,3 +28,31 @@ async function carregarBiblia() {
     return biblia;
 
 }
+
+async function obterLivro(abrev) {
+
+    const biblia = await carregarBiblia();
+
+    return biblia.find(livro => livro.abbrev === abrev);
+
+}
+
+async function obterCapitulo(abrev, numero) {
+
+    const livro = await obterLivro(abrev);
+
+    if (!livro) return null;
+
+    return livro.chapters[numero - 1];
+
+}
+
+async function totalCapitulos(abrev) {
+
+    const livro = await obterLivro(abrev);
+
+    if (!livro) return 0;
+
+    return livro.chapters.length;
+
+}
